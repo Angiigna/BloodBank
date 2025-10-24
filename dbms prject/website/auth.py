3499007d69408ec3913010ca6fa95123a89f1636
Angigna <sivangisankar19@gmail.com>
2025-10-24 12:05:54 +0530

    made 5 basic html templates

--- dbms prject/website/auth.py
+++ dbms prject/website/auth.py
@@ -1,35 +1,39 @@
 # step 5 Now add the blueprints for auth also
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route("/login")
 def login():
-    return "<h1> Login </h1>"
+    return render_template("login.html")
 
 
 @auth.route("/logout")
 def logout():
-    return "<h1> Logout </h1>"
+    return render_template("login.html", boolean=True)
+
+@auth.route("/signup")
+def signup():
+    return render_template("signup.html")
 
 
-@auth.route("register_donor")
+@auth.route("/register_donor")
 def register_donor():
-    return "<h1> Register Donor </h1>"
+    return render_template("register_donor.html")
 
 
-@auth.route("filter_donors")
+@auth.route("/filter_donors")
 def filter_donors():
-    return "<h1> Filter Donors </h1>"
+    return render_template("filter_donors.html")
 
 
-@auth.route("view_requests")
+@auth.route("/view_requests")
 def view_requests():
-    return "<h1> View Requests </h1>"
+    return render_template("view_requests.html")
 
 
-@auth.route("make_request")
+@auth.route("/make_request")
 def make_request():
-    return "<h1> Make Request </h1>"
+    return render_template("make_request.html")
